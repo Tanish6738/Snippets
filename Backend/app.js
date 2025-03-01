8dbc09e3f3ede8f9f049ccb1b06285727d75bf89
Tanish6738 <123645393+Tanish6738@users.noreply.github.com>
2025-03-01 14:51:42 +0530

    still have some errors

--- Backend/app.js
+++ Backend/app.js
@@ -39,7 +39,8 @@ app.use(cors({
         'Date',
         'X-Api-Version',
         'Origin',
-        'Cache-Control'
+        'Cache-Control',
+        'Pragma'
     ]
 }));
 
@@ -48,7 +49,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://snippets-frontend-pearl.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
     
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
